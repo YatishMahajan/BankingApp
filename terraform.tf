e0a9097779f364002dc07133e8c5de62bb27a7e8
YatishMahajan <mahajanyatish0307@gmail.com>
2024-07-16 17:44:20 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Prod_Server" {
   ami = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  subnet_id = "0ad8de5c6ad8a3734"
+  subnet_id = "subnet-0ad8de5c6ad8a3734"
   vpc_security_group_ids = ["sg-09f31579683c3fb4a"]
   key_name = "securitykey2"
   tags = {
